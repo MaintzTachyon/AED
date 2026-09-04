744d05aaecccdd6f9707ec76d89a8a611260f391
Juan Diego Mejia Armas <juan.mejia@utec.edu.pe>
2026-09-04 00:29:14 -0500

    implementacion de linked list

--- Semana 3/Codeforces3/ejercicio1.cpp
+++ Semana 3/Codeforces3/ejercicio1.cpp
@@ -9,9 +9,9 @@ class doublelinkedlist{
         Node* prev;
 
         Node(const T& val, Node* p, Node* n) : data (val), next(n), prev(p) {}
-    }
-    Node * head;
-    Node * tail;
+    };
+    Node* head = nullptr;
+    Node* tail = nullptr;
     public:
         void insert(const T& val){
             Node* newNode = new Node(val, nullptr, head);
@@ -29,12 +29,61 @@ class doublelinkedlist{
             while(curr!= nullptr){
                 if (curr->data == val){}
                     unlink(curr);
+                    delete curr;
                 }
-            }
+                curr = curr->next;
         }
-}
+
+        void deleteFirst(){
+            if (head == nullptr) return;
+            Node* temp = head;
+            unlink(temp);
+            delete temp;
+        }
+
+        void deleteLast(){
+            if (tail == nullptr)return;
+            Node* temp = tail;
+            unlink(temp);
+            delete temp;
+        }
+
+        void print(std::ostream& out)const{
+            Node* curr = head;
+            bool first = true;
+            while(curr != nullptr){
+                if (!first) out << " ";
+                out << curr->data;
+                first = false;
+                curr = curr->next;
+            }
+            out << '\n';
+        }
+
+        ~doublelinkedlist(){
+            Node* curr = head;
+            while(curr != nullptr){
+                Node* next = curr->next;
+                delete curr;
+                curr = next;
+            }
+        }  
+    private:
+        void unlink(Node* node){
+            if (node->prev != nullptr) node->prev->next = node->next;
+            else head = node->next;
+
+            if (node->next != nullptr) node->next->prev = node->prev;
+            else tail = node->prev;
+        }
+
+};
+
 
 int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     int n;
     std::cin >> n;
 
@@ -57,7 +106,8 @@ int main(){
             list.deleteLast();
         }
     }
-    list.print();
+    
+    list.print(std::cout);
 
     return 0;
 }

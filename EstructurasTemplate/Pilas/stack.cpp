7863f9c0d71b7601193f2223fdd59c4eacb6e51d
Juan Diego Mejia Armas <juan.mejia@utec.edu.pe>
2026-09-10 16:24:35 -0500

    implementacion de stack v2

--- EstructurasTemplate/Pilas/stack.cpp
+++ EstructurasTemplate/Pilas/stack.cpp
@@ -1,26 +1,56 @@
 #include <iostream>
+#include <stdexcept>
+#include <cstddef>
 
 template<typename T>
 class Stack{
     struct Node{
         T data;
         Node* next = nullptr;
-
         Node(const T& d, Node* n = nullptr): data(d), next(n){}
     };
     Node* top = nullptr;
+    std::size_t count = 0;
+
     public:
 
+    Stack() = default;
+
+    Stack(const Stack&) = delete;
+    Stack& operator=(const Stack&) = delete;
+
     void push(const T& val){
-        Node* newNode = new Node();
-        newNode->data = val;
-        newNode->next = top;
-        top = newNode;
+        top = new Node(val, top);
+        ++count;
     }
 
     void pop(){
+        if (top == nullptr)return;
+        
         Node*temp = top;
         top = top->next;
         delete temp;
+        --count;
+    }
+
+    const T& peek()const{
+        if(topp == nullptr) throw std::runtime_error("Stack is empty");
+
+        return top->value;
+    }
+
+
+    std::size_t size(){
+        return count;
+    }
+
+    bool isEmpty(){
+        return top == nullptr;
+    }
+
+    ~Stack(){
+        while(top != nullptr){
+            pop();
+        }
     }
 };
